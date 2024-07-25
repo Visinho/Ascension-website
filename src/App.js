27b28f585453ed0a4d4ab2a0523e5dc7f4bf2d9b
Visinho <Elvisvisinho@rocketmail.com>
2024-07-25 13:06:01 +0100

    Navbar logic complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-  
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact Component={Home} />
+        </Routes>
+      </Router>
     </div>
   );
 }
