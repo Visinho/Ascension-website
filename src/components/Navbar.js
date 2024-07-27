0141615ec766b80d0e13fa1f934fda2a98c4924e
Visinho <Elvisvisinho@rocketmail.com>
2024-07-27 19:14:18 +0100

    Responsive design WIP

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,29 +12,50 @@ const Navbar = () => {
         setOpenLinks(!openLinks);
     }
 
-  return (
-    <div className='navbar'>
-      <div className='leftSide' id={openLinks ? "open" : "close"}>
-        <img src={Logo} alt='' />
-        <h1>The Church of Nigeria (Anglican Communion)</h1>
-        <div className='hiddenLinks'>
-        <Link to="/">Home</Link>
-        <Link to="/programs">Programs</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-        </div>
-      </div>
-      <div className='rightSide'>
-        <Link to="/">Home</Link>
-        <Link to="/programs">Programs</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-        <button onClick={toggleNavbar}>
-            <ReorderIcon />
-        </button>
-      </div>
+//   return (
+//     <div className='navbar'>
+//       <div className='leftSide' id={openLinks ? "open" : "close"}>
+//         <img src={Logo} alt='Church Logo' />
+//         <h1>The Church of Nigeria (Anglican Communion)</h1>
+//         <div className='hiddenLinks'>
+//         <Link to="/">Home</Link>
+//         <Link to="/programs">Programs</Link>
+//         <Link to="/about">About</Link>
+//         <Link to="/contact">Contact</Link>
+//         </div>
+//       </div>
+//       <div className='rightSide'>
+//         <Link to="/">Home</Link>
+//         <Link to="/programs">Programs</Link>
+//         <Link to="/about">About</Link>
+//         <Link to="/contact">Contact</Link>
+//         <button onClick={toggleNavbar}>
+//             <ReorderIcon />
+//         </button>
+//       </div>
+//     </div>
+//   )
+// }
+
+return (
+  <div className='navbar'>
+    <div className='leftSide'>
+      <img src={Logo} alt='Church Logo' />
+      <h1>The Church of Nigeria (Anglican Communion)</h1>
     </div>
-  )
+    <div className='rightSide'>
+      <div className={`links ${openLinks ? "open" : ""}`}>
+        <Link to="/">Home</Link>
+        <Link to="/programs">Programs</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+      <button onClick={toggleNavbar}>
+        <ReorderIcon />
+      </button>
+    </div>
+  </div>
+);
 }
 
 export default Navbar
